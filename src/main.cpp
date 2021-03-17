3a03614d1bd5442a7e15b6ad0289561ebc7fa062
Дмитрий Кушнарюк <dkushnaryuk@diasoft.ru>
2021-03-17 21:07:08 +0300

    META-01 | rewrite realization of print_ip for arithmetic types and stl containers. If stl container is std::string output as is

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,46 @@
-//
-// Created by dim on 3/15/21.
-//
-#include <vector>
-#include <algorithm>
-#include <iostream>
-#include <list>
-#include <type_traits>
+// Реализовать функцию печати условного ip-адреса.
+// функция печати должна быть одной шаблонной функцией с частичными специализациями
 
-template<typename isDecType>
-  struct type_is{
-      using type = isDecType;
+#include <iostream>
+#include <type_traits>
+#include <vector>
+#include <list>
+#include <string>
+#include <algorithm>
+
+template<class T>
+struct type_is{
+    using type = T;
 };
 
+template<class T>
+struct is_stl_container :type_is<T>{
+    static const bool value = false;
+};
+
+template<>
+struct is_stl_container<std::vector<int>> : type_is<std::vector<int>>{
+    static const bool value = true;
+};
+
+template<>
+struct is_stl_container<std::list<int>> : type_is<std::list<int>>{
+    static const bool value = true;
+};
+
+template<>
+struct is_stl_container<std::string> : type_is<std::string>{
+    static const bool value = true;
+};
+
+template<class T>
+    auto is_stl_container_v = is_stl_container<T>::value;
+
+
 template<typename T>
-struct is_dec : type_is<T> {};
-
-template<>
- struct is_dec<int> : type_is<int>{
-     using type = int;
- };
-
-template<>
-struct is_dec<long> : type_is<long>{};
-
-template<>
-struct is_dec<short> : type_is<short>{};
-
-template<>
-struct is_dec<char> : type_is<char>{};
-
-
-template<int num, typename T>
-struct check_type : type_is<T>{};
-
-template<typename T>
-struct check_type<1,T> : type_is<is_dec<T>>{};
-
-
-template<typename T, typename dec_type = check_type<1,T>>
-void print_ip(dec_type arg){
+void print_ip(std::enable_if_t<std::is_arithmetic_v<T>, T> const && arg ){
     std::vector<int> ipAddress;
-    for(auto i = 0, bytes_shift = 8 ; i < sizeof(typename is_dec<T>::type) ; i++){
+    for(auto i = 0, bytes_shift = 8 ; i < sizeof(T) ; i++){
         if(i == 0)
             ipAddress.emplace_back(arg & 0xFF);
         else{
@@ -62,18 +62,32 @@ void print_ip(dec_type arg){
 }
 
 
-//template<typename T, typename container_type = check_type<2,T>>
-//void print_ip(container_type arg){}
+template<typename T>
+void print_ip(std::enable_if_t<is_stl_container<T>::value,T> const && arg){
+    if constexpr (std::is_same_v<T, std::string>){
+        std::cout << arg << std::endl;
+        return;
+    }
 
+    for(auto it = arg.begin(); it != arg.end(); it++){
+        if(it != std::prev(arg.end()))
+            std::cout << *it << ".";
+        else
+            std::cout << *it << std::endl;
+    }
 
+}
 
 int main(){
     print_ip<char>(-1);
     print_ip<short>(0);
     print_ip<int>(2130706433);
     print_ip<long>(8875824491850138409);
-    //print_ip<char*>("8875824491850138409");
-    //print_ip<std::vector<int>>(std::vector<int>{10,23,4,56,5});
+
+    print_ip<std::vector<int>>(std::vector<int>{10,23,4,56,5});
+    print_ip<std::list<int>>(std::list<int>{115,20,41,0});
+    print_ip<std::string>(std::string{"10.10.20.20"});
+
 
     return 0;
 }
